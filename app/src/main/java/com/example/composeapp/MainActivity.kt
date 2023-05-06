f53c1141f9355d2555aff198692efbffcf5c4d62
satoken7 <satoken3110@icloud.com>
2023-05-06 15:37:19 +0900

    関数に分けよう

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
+import java.util.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +29,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Push me!!")
-                        
-                    }
+                    MainScreen()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        var isAndroid by remember {
+            mutableStateOf(true)
+        }
+        var dateText by remember {
+            mutableStateOf("")
+        }
+        if (isAndroid) {
+            Greeting(name = "Android")
+        } else {
+            Greeting(name = "iPhone")
+        }
+        Button(onClick = {
+            isAndroid = !isAndroid
+        }) {
+            Text(text = "Push me!!")
+        }
+        Text(text = dateText)
+        Button(onClick = {
+            val date = Date()
+            dateText = date.toString()
+        }) {
+            Text(text = "Time!")
+
         }
     }
 }
@@ -43,6 +77,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+       MainScreen()
     }
 }
