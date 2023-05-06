29ddb90ca6a6f5b21219633447bf4298e145975f
satoken7 <satoken3110@icloud.com>
2023-05-06 18:09:23 +0900

    ViewModelに処理移動、時刻を毎秒表示

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.composeapp
 
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.text.format.DateFormat
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -13,12 +11,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
-import java.util.*
 
 
 class MainActivity : ComponentActivity() {
@@ -43,11 +41,9 @@ fun MainScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.v
     Column(modifier = Modifier
         .padding(16.dp)
         .fillMaxWidth()) {
-        var isAndroid = viewModel.isAndroid.collectAsState()
+        val isAndroid = viewModel.isAndroid.collectAsState()
 
-        var dateText by remember {
-            mutableStateOf("")
-        }
+        val dateText = viewModel.dateText.collectAsState()
         if (isAndroid.value) {
             Greeting(name = "Android")
         } else {
@@ -58,23 +54,9 @@ fun MainScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.v
         }) {
             Text(text = "Push me!!")
         }
-        Text(text = dateText)
-        Button(onClick = {
-            dateText = getDataText()
-        }) {
-            Text(text = "Time!")
-
-        }
+        Text(text = dateText.value)
     }
 }
-
-private fun getDataText() :String {
-    val date = Date()
-    val local = Locale.getDefault()
-    val pattern = DateFormat.getBestDateTimePattern(local,"yyyyMMddHHmmss")
-    return SimpleDateFormat(pattern,local).format(date)
-}
-
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
--- app/src/main/java/com/example/composeapp/MainViewModel.kt
+++ app/src/main/java/com/example/composeapp/MainViewModel.kt
@@ -1,16 +1,44 @@
 package com.example.composeapp
 
+import android.icu.text.SimpleDateFormat
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.*
 
 class MainViewModel : ViewModel() {
     private val _isAndroid = MutableStateFlow(true)
     val isAndroid : StateFlow<Boolean> = _isAndroid
 
+    private val _dateText = MutableStateFlow("")
+    val dateText : StateFlow<String> = _dateText
+
     init {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                while (true) {
+                    _dateText.value = getDataText()
+                    delay(1000)
+                }
+            }.onFailure {
+                Log.w("tag",it)
+            }
+        }
 
     }
+
+    private fun getDataText() :String {
+        val date = Date()
+        val local = Locale.getDefault()
+        val pattern = DateFormat.getBestDateTimePattern(local,"yyyyMMddHHmmss")
+        return SimpleDateFormat(pattern,local).format(date)
+    }
+
 
     fun onClick() {
         _isAndroid.value = !_isAndroid.value
