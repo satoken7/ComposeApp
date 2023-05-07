f9e64a69896841badb9ac3a314a32d92a86d5b41
satoken7 <satoken3110@icloud.com>
2023-05-07 16:47:16 +0900

    ダイアログ表示とあれこれ

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,6 +37,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+const val TAG = "ログのタグ"
 
 @Composable
 fun MainScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
@@ -84,8 +84,51 @@ fun HomeScreen(
 }
 
 @Composable
-fun CommScreen(){
-    Greeting(name = "CommScreen")
+fun CommScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        Greeting(name = "CommScreen")
+        val login = viewModel.login.collectAsState()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = login.value)
+            Button(
+                onClick = { viewModel.onGet()}
+            ) {
+                Text(text = "onGet!!")
+            }
+        }
+        Divider()
+        val names = listOf("microsoft", "apple", "google")
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)){
+            items(names) {
+                Row(modifier = Modifier
+                    .clickable { viewModel.onSelect(it) }
+                    .fillMaxWidth()) {
+                    Text(text = it)
+                }
+            }
+        }
+
+    }
+    val inProgress = viewModel.inProgress.collectAsState()
+    if (inProgress.value) {
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    }
+    val message = viewModel.message.collectAsState()
+    if (message.value.isNotEmpty()) {
+        AlertDialog(
+            onDismissRequest = { viewModel.onDismiss() },
+            buttons = {},
+            text = { Text(text = message.value) })
+    }
 }
 @Composable
 fun Greeting(name: String) {
--- app/src/main/java/com/example/composeapp/MainViewModel.kt
+++ app/src/main/java/com/example/composeapp/MainViewModel.kt
@@ -5,6 +5,8 @@ import android.text.format.DateFormat
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,8 +17,17 @@ class MainViewModel : ViewModel() {
     private val _isAndroid = MutableStateFlow(true)
     val isAndroid : StateFlow<Boolean> = _isAndroid
 
+    private val _inProgress = MutableStateFlow(false)
+    val inProgress : StateFlow<Boolean> = _inProgress
+
     private val _dateText = MutableStateFlow("")
     val dateText : StateFlow<String> = _dateText
+
+    private val _message = MutableStateFlow("")
+    val message : StateFlow<String> = _message
+
+    private val _login = MutableStateFlow("")
+    val login : StateFlow<String> = _login
 
     init {
         viewModelScope.launch {
@@ -26,7 +37,7 @@ class MainViewModel : ViewModel() {
                     delay(1000)
                 }
             }.onFailure {
-                Log.w("tag",it)
+                Log.w(TAG,it)
             }
         }
 
@@ -43,6 +54,36 @@ class MainViewModel : ViewModel() {
     fun onClick() {
         _isAndroid.value = !_isAndroid.value
     }
+    fun onDismiss() {
+        _message.value = ""
+    }
+    fun onSelect(name: String) {
+        _login.value = name
+    }
 
-
+    fun onGet() {
+        if(_inProgress.value) {
+            Log.d(TAG,"プログレス")
+            return
+        } else {
+            _inProgress.value = true
+        }
+        viewModelScope.launch {
+            kotlin.runCatching {
+                val (request, response, result) = Fuel.get("https://api.github.com/users/${_login.value}")
+                    .awaitStringResponseResult()
+                Log.d(TAG,"request=$request")
+                Log.d(TAG,"response=$response")
+                Log.d(TAG,"result=$result")
+                result.get()
+            }.onSuccess {
+                _message.value = it
+            }.onFailure {
+                Log.e(TAG,"onFailureですよ$it")
+                _message.value = it.message.toString()
+            }.also {
+                _inProgress.value = false
+            }
+        }
+    }
 }
